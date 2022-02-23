47d676eb872b02484095b48ef7ce6ccbd1b6cf35
Todd Wang <toddwang@gmail.com>
2022-02-23 13:39:27 -0800

    Fix UploadResult.Predominant and add test (#2)
    * Fix UploadResult.Predominant and add test
    
    * more
    
    * more

--- api/uploader/upload_asset.go
+++ api/uploader/upload_asset.go
@@ -105,10 +105,10 @@ func (u *API) Upload(ctx context.Context, file interface{}, uploadParams UploadP
 	}
 
 	body, err := u.postFile(ctx, file, formParams)
-
 	if err != nil {
 		return nil, err
 	}
+
 	upload := &UploadResult{}
 	err = json.Unmarshal(body, upload)
 	if err != nil {
@@ -191,7 +191,7 @@ type UploadResult struct {
 	Moderation            []Moderation                  `json:"moderation,omitempty"`
 	Overwritten           bool                          `json:"overwritten"`
 	Colors                []ColorWeight                 `json:"colors,omitempty"`
-	Predominant           map[string]ColorWeight        `json:"predominant,omitempty"`
+	Predominant           map[string][]ColorWeight      `json:"predominant,omitempty"`
 	Phash                 string                        `json:"phash,omitempty"`
 	OriginalFilename      string                        `json:"original_filename"`
 	Eager                 []Eager                       `json:"eager"`
